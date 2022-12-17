3f0535c7f97f89ee8232ec1228ce9ecb16b76428
tom-gnade <tom.gnade@node-ops.com>
2022-12-17 12:34:50 -0500

    Create algorand-monitoring-tools-installer.sh
    Initial checkin of script with no logic implemented.

---
+++ algorand-monitoring-tools-installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Performs a multi-step installation of Prometheus, Node Exporter, Grafana, Algorand dashboards
+# Intended to simplify monitoring tool installation for node runners
+# Tested on Ubuntu 20.04.5 LTS - compatibility with other operating systems has not been verified
+# REF: https://github.com/ava-labs/avalanche-monitoring/blob/main/grafana/monitoring-installer.sh
+# REF: https://linuxopsys.com/topics/install-prometheus-on-ubuntu
+
+#stop on errors
+set -e
+
+exit 0
