c286f7abc8da62113bfefdcc7d3a907338380d70
tom-gnade <tom.gnade@gmail.com>
2024-02-11 11:21:26 -0500

    startup script for api-caller

--- api-caller/startup.sh
+++ api-caller/startup.sh
@@ -1,5 +1,5 @@
 # -~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-
-# !/bin/bash
+# !/bin/bash 
 
 curl -XPUT -H "Content-Type: application/json" "http://localhost:9200/_template/template_1" \
   -d '{"index_patterns" : ["*"], "order": 0, "settings": {"number_of_shards": 1, "number_of_replicas": 0}}';
