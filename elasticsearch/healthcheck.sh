d5578bf55b1ee86dd14c5cdcd47ff4f852f13e5e
Tom Gnade <tom.gnade@gmail.com>
2024-02-11 13:08:19 -0500

    Attempting to preserve all changes made on macbook yesterday that were not properly committed.

--- elasticsearch/healthcheck.sh
+++
@@ -1,6 +0,0 @@
-# -~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-
-# !/bin/bash
-
-(curl -XPUT -H "Content-Type: application/json" "http://localhost:9200/_template/template_1" \
-  -d '{"index_patterns" : ["*"], "order": 0, "settings": {"number_of_shards": 1, "number_of_replicas": 0}}' && \
- curl -s http://localhost:9200/_cat/health | grep -q "green\|yellow") || exit 1;
